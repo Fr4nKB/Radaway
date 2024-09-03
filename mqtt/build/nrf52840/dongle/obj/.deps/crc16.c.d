26e6c0431e7c7fd1fa5b0673f67d12b555a26b97
iot_ubuntu_intel <iot_ubuntu_intel@iot-ubuntu-intel.myguest.virtualbox.org>
2024-09-03 21:30:11 +0200

    database and coordinator fix

--- mqtt/build/nrf52840/dongle/obj/.deps/crc16.c.d
+++
@@ -1,2 +0,0 @@
-build/nrf52840/dongle/obj/crc16.o: \
- /home/iot_ubuntu_intel/contiki-ng/os/lib/crc16.c
--- mqtt/build/nrf52840/dongle/obj/.deps/syscalls.c.d
+++
@@ -1,2 +0,0 @@
-build/nrf52840/dongle/obj/syscalls.o: \
- /home/iot_ubuntu_intel/contiki-ng/os/lib/newlib/syscalls.c
